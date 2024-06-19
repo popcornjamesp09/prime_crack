5d3b0ae80ea0e73c5b9f99aa492669e5be8831d3
James <popcornjamesp09@gmail.com>
2024-06-19 19:51:43 -0400

    Added comments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-//use std::io;
 use rand::{self, Rng};
 
 fn main() {
-    let prime: u128 = 727*521;
+    // Calculates the number that will be cracked
+    let prime: u128 = 727*521; 
     let random_number:u128 = rand::thread_rng().gen_range(1..=prime/2);
 
     let mut working_guess: u128 = random_number.clone();
     let guess: u128 = working_guess.clone();
     let mut power: u32 = 2;
+    
+    // Raises the random number to a power and keeps going untill the remainder is one
     while working_guess % prime != 1{
         working_guess = u128::pow(working_guess,power);
         power +=1;
@@ -17,6 +19,7 @@ fn main() {
     let mut number_b: u128 = u128::pow(guess, power/2)+1;
     let mut total: bool = true;
 
+    // Finds the GCD using Euclidean's Algorithm 
     while total {
         if number_a > number_b {
             number_a = number_a % number_b;
