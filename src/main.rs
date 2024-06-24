a0fe0358c1bbe5b608c55615ceca8e6263c831c6
James <popcornjamesp09@gmail.com>
2024-06-24 18:56:36 -0400

    Added args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use rand::{self, Rng};
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: prime_crack <prime1> <prime2>");
+        println!("       prime1  - The first prime number that you want");
+        println!("       prime2  - The second prime number that you want");
+        println!("This program finds the two prime numbers provided after multiplying them together.");
+    }
+    
+    let arg_prime_1: u128 = args[1].trim().parse().expect("Enter a number");
+    let arg_prime_2: u128 = args[2].trim().parse().expect("Enter a number");
+
     // Calculates the number that will be cracked
-    let prime: u128 = 727*521; 
+    let prime: u128 = arg_prime_1*arg_prime_2; 
     let random_number:u128 = rand::thread_rng().gen_range(1..=prime/2);
 
-    let mut working_guess: u128 = random_number.clone();
-    let guess: u128 = working_guess.clone();
+    let guess: u128 = random_number;
+    let mut working_guess: u128 = guess;
     let mut power: u32 = 2;
     
     // Raises the random number to a power and keeps going untill the remainder is one
@@ -16,29 +29,25 @@ fn main() {
     }
 
     let number_a: u128 = prime;
-    let number_b: u128 = u128::pow(guess, power/2)+1;
+    let number_b: u128 = u128::pow(guess, {power/2}.try_into().unwrap())+1;
+
 
     find_gcl(prime, number_a, number_b);
 }
 
 // Finds the GCD using Euclidean's Algorithm 
 fn find_gcl(prime: u128, mut number_a: u128, mut number_b: u128) {
-    let mut total: bool = true;
-    while total {
-        if number_a > number_b {
-            number_a = number_a % number_b;
-            if number_a == 0 {
-                get_primes_from_guess(prime, number_b);
-                total = false;
-            }
-        } else if number_b > number_a {
-            number_b = number_b % number_a;
-            if number_b == 0 {
-                get_primes_from_guess(prime, number_a);
-                total = false;
-            }
+    if number_a > number_b {
+        number_a = number_a % number_b;
+        if number_a == 0 {
+            get_primes_from_guess(prime, number_b);
         }
+    } else if number_b > number_a {
+        number_b = number_b % number_a;
+        if number_b == 0 {
+            get_primes_from_guess(prime, number_a);
     }
+}
 }
 
 fn get_primes_from_guess(prime: u128,guess: u128) {
